4ac7f37f97a46e134dd2e043f3bc9a60b82da771
Luka <luka.stojcev.2023@student.lhschools.org>
2020-02-24 14:16:42 -0500

    Intellij Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println();
+        System.out.println("Three Things I Did Over Break");
+        System.out.println("1. I hung out with friends.");
+        System.out.println("2. I went on a Christmas lunch with my parents.");
+        System.out.println("3. I got a dartboard.");
+    }
+}
